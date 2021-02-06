b9791c1dc901cbac9a808d3690cb2fc0075687d2
RoksolianaMatseliukh <matseliukhroksoliana100@gmail.com>
2021-02-07 00:54:09 +0200

    message

--- src/configuration/index.js
+++ src/configuration/index.js
@@ -1,6 +1,6 @@
 export const app_configuration = {
     API_URL: process.env.REACT_APP_API_URL || 'https://api.nasa.gov',
     API_KEY: process.env.REACT_APP_API_KEY || 'eJLxI5oEvp7afbJ45uzmk7nLtgBOLD9UdU0qwmix',
-    SENTRY_DSN: process.env.REACT_APP_SENTRY_DSN || 'your sentry dsn'
+    SENTRY_DSN: process.env.REACT_APP_SENTRY_DSN || 'https://12a3b9f402954497902fa05066ba0f41@o505578.ingest.sentry.io/5594162'
 };
 
