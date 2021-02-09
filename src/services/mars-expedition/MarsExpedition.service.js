541cf2e9975bfaded76adf07b4ea91cb12b838b2
RoksolianaMatseliukh <matseliukhroksoliana100@gmail.com>
2021-02-09 02:27:22 +0200

    message

--- src/services/mars-expedition/MarsExpedition.service.js
+++ src/services/mars-expedition/MarsExpedition.service.js
@@ -9,6 +9,7 @@ class MarsExpeditionService {
 
     async loadImages(rover, sol, camera) {
         const response = await axios.get(`${API_URL}/mars-photos/api/v1/rovers/${rover}/photos?sol=${sol}&camera=${camera}&api_key=${API_KEY}`);
+        console.log(response)
         return response.data;
     };
 }
